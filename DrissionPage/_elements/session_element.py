3250292ac0bc75ec47650483b612788934d2d52d
wenming21 <wen-ming21@qq.com>
2024-12-31 01:31:22 +0800

    优化_get_ele_path

--- DrissionPage/_elements/session_element.py
+++ DrissionPage/_elements/session_element.py
@@ -149,26 +149,11 @@ class SessionElement(DrissionElement):
     def _find_elements(self, locator, timeout, index=1, relative=False, raise_err=None):
         return make_session_ele(self, locator, index=index)
 
-    def _get_ele_path(self, xpath=True):
-        path_str = ''
-        ele = self
-
-        if xpath:
-            while ele:
-                brothers = len(ele.eles(f'xpath:./preceding-sibling::{ele.tag}'))
-                path_str = f'/{ele.tag}[{brothers + 1}]{path_str}'
-                ele = ele.parent()
-
-        else:
-            while ele:
-                id_ = ele.attr('id')
-                if id_:
-                    path_str = f'>{ele.tag}#{id_}{path_str}'
-                else:
-                    path_str = f'>{ele.tag}:nth-child({len(ele.eles("xpath:./preceding-sibling::*")) + 1}){path_str}'
-                ele = ele.parent()
-
-        return path_str if xpath else f'{path_str[1:]}'
+    def _get_ele_path(self, xpath = True):
+        HtmlElement = self._inner_ele
+        xpath_path = HtmlElement.getroottree().getpath(HtmlElement)
+        return xpath_path if xpath else (xpath_path.lstrip('/').replace('[', ':nth-of-type(')
+                                         .replace(']', ')').replace('/', '>'))
 
 
 def make_session_ele(html_or_ele, loc=None, index=1, method=None):
